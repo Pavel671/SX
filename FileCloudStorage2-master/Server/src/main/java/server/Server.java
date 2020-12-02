d85c77caa28f6907f221f4d76f2883db49611d8a
pavel <pavel671@bk.ru>
2020-12-03 00:08:55 +0300

    server

---
+++ FileCloudStorage2-master/Server/src/main/java/server/Server.java
@@ -0,0 +1,66 @@
+package server;
+
+import common.Constants;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.serialization.ClassResolvers;
+import io.netty.handler.codec.serialization.ObjectDecoder;
+import io.netty.handler.codec.serialization.ObjectEncoder;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+
+public class Server {
+
+    private final int port;
+    public static Map<Integer, Long> connectionUsersMap = new ConcurrentHashMap<>();
+
+    public Server(int port) {
+        this.port = port;
+    }
+
+    public void run() throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel socketChannel) {
+                            socketChannel.pipeline().addLast(
+                                    new ObjectDecoder(1024 * 1024 * 100 * 2, ClassResolvers.cacheDisabled(null)),
+                                    new ObjectEncoder(),
+                                    new MainHandler()
+                                    );
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+
+            ChannelFuture f = b.bind(port).sync(); // (7)
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = Constants.serverPort;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        }
+        new Server(port).run();
+    }
+    
+}

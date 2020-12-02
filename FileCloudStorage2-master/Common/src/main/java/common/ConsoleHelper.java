8c7205c90cb8f8d5276971f4c51aa75a09eb7aa4
pavel <pavel671@bk.ru>
2020-12-02 23:56:19 +0300

    Common

---
+++ FileCloudStorage2-master/Common/src/main/java/common/ConsoleHelper.java
@@ -0,0 +1,34 @@
+package common;
+
+import common.exception.InvalidInputFormatException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class ConsoleHelper {
+    private static final BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static void writeError(String message) {
+        System.err.println(message);
+    }
+
+    public static String readString() throws IOException {
+        return bis.readLine();
+    }
+
+    public static int readInt() throws IOException, InvalidInputFormatException {
+        try {
+            String text = readString();
+            return Integer.parseInt(text.trim());
+        } catch (NumberFormatException e){
+            writeMessage("Не корректный ввод");
+            throw new InvalidInputFormatException();
+        }
+    }
+}
